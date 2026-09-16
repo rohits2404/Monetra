7c7f5ea7c5acb335ff3bc0af6d864d5631a5e1d1
Rohit <rohits8731@gmail.com>
2026-09-16 12:09:40 +0530

    24: Settings Page

---
+++ src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,12 @@
+import { SettingsCard } from "@/features/settings";
+import React from "react";
+
+const SettingsPage = () => {
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <SettingsCard />
+        </div>
+    );
+};
+
+export default SettingsPage;
